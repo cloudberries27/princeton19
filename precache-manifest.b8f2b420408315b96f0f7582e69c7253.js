6e57786cd4ae7b4f94a9e6dfb030166ef53ade35
cloudberries27 <ccrod2298@gmail.com>
2019-11-10 09:16:45 -0500

    Updates

---
+++ precache-manifest.b8f2b420408315b96f0f7582e69c7253.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a9ce4cea570abcf35d6e18db47c9ae",
+    "url": "/princeton19/index.html"
+  },
+  {
+    "revision": "be1f2b489eaf8e823017",
+    "url": "/princeton19/static/css/main.bf5c9e46.chunk.css"
+  },
+  {
+    "revision": "6ba3a2d91d288da5e4b4",
+    "url": "/princeton19/static/js/2.5c4486e4.chunk.js"
+  },
+  {
+    "revision": "be1f2b489eaf8e823017",
+    "url": "/princeton19/static/js/main.0dfa6b2c.chunk.js"
+  },
+  {
+    "revision": "5c7c700a71037de36473",
+    "url": "/princeton19/static/js/runtime-main.bea833d4.js"
+  },
+  {
+    "revision": "dd14e2672c3a6dc86bf00bdd29e0a904",
+    "url": "/princeton19/static/media/claudia.dd14e267.jpg"
+  },
+  {
+    "revision": "c3ae86d4346feede5831548dab6818a2",
+    "url": "/princeton19/static/media/denise.c3ae86d4.png"
+  },
+  {
+    "revision": "14c590d124662eb50efa4c00f027b79c",
+    "url": "/princeton19/static/media/fa-brands-400.14c590d1.eot"
+  },
+  {
+    "revision": "3e1b2a654a784ceb385157140b4ccd71",
+    "url": "/princeton19/static/media/fa-brands-400.3e1b2a65.woff2"
+  },
+  {
+    "revision": "5e8aa9ea0ebcd2218178f554cdd8e6b0",
+    "url": "/princeton19/static/media/fa-brands-400.5e8aa9ea.ttf"
+  },
+  {
+    "revision": "bfa9c38bd6081dafe7278dedc4aad1d9",
+    "url": "/princeton19/static/media/fa-brands-400.bfa9c38b.svg"
+  },
+  {
+    "revision": "df02c782834b113d605d8329368737b4",
+    "url": "/princeton19/static/media/fa-brands-400.df02c782.woff"
+  },
+  {
+    "revision": "285a9d2a28886ad64c4e45cbd733cf7c",
+    "url": "/princeton19/static/media/fa-regular-400.285a9d2a.ttf"
+  },
+  {
+    "revision": "5623624dd1b017c66c29d1ac69cdcfa3",
+    "url": "/princeton19/static/media/fa-regular-400.5623624d.woff"
+  },
+  {
+    "revision": "95f13e0be408d117bd3d9b366084a3ef",
+    "url": "/princeton19/static/media/fa-regular-400.95f13e0b.svg"
+  },
+  {
+    "revision": "aa66d0e0e38c75666e98db33abae955e",
+    "url": "/princeton19/static/media/fa-regular-400.aa66d0e0.eot"
+  },
+  {
+    "revision": "ac21cac3f22cc9642f5af32e0c750797",
+    "url": "/princeton19/static/media/fa-regular-400.ac21cac3.woff2"
+  },
+  {
+    "revision": "3ded831d708bf90b2da86756551b5c1c",
+    "url": "/princeton19/static/media/fa-solid-900.3ded831d.woff"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "/princeton19/static/media/fa-solid-900.42e1fbd2.eot"
+  },
+  {
+    "revision": "6ed5e3bc9018d2a03e2f1fadbf8a0a4a",
+    "url": "/princeton19/static/media/fa-solid-900.6ed5e3bc.svg"
+  },
+  {
+    "revision": "896e20e26ad07dc63c9526ed814117e9",
+    "url": "/princeton19/static/media/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "d6d8d5da9214dc7d46b297672a602d55",
+    "url": "/princeton19/static/media/fa-solid-900.d6d8d5da.woff2"
+  },
+  {
+    "revision": "072cba28cc90acda080a02ab963f2244",
+    "url": "/princeton19/static/media/john.072cba28.png"
+  },
+  {
+    "revision": "60635e4395598ff460a6bd9ed4b76986",
+    "url": "/princeton19/static/media/jordan.60635e43.png"
+  },
+  {
+    "revision": "49c428a346b6ab664af8bc430fc8bb3c",
+    "url": "/princeton19/static/media/serenaBig.49c428a3.jpg"
+  },
+  {
+    "revision": "41bf8f008ca64ea7f5175c965f60de81",
+    "url": "/princeton19/static/media/steph.41bf8f00.png"
+  },
+  {
+    "revision": "fbee8862918d7229f364d94bfe802214",
+    "url": "/princeton19/static/media/yang.fbee8862.png"
+  }
+]);
